c27dbbf054be821d6a89bdd4ec4a55fd232adf94
NeranjayaDilshan <neranjayadilshan@gmail.com>
2024-07-14 16:17:10 +0530

    get Customer values

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class CustomerController {
-     
+
     @FXML
     private TextField txtAddress;
 
@@ -20,6 +20,16 @@ public class CustomerController {
 
     @FXML
     void btnSaveCustomerOnAction(ActionEvent event) {
+        System.out.println("Save Customer Btn");
+        int id = Integer.parseInt(txtCustomerID.getText());
+        String name = txtCustomerName.getText();
+        String address = txtAddress.getText();
+        double salary = Double.parseDouble(txtSalary.getText());
+
+        System.out.println("ID : "+id);
+        System.out.println("Name : "+name);
+        System.out.println("Address : "+address);
+        System.out.println("Salary : "+salary);
 
     }
 

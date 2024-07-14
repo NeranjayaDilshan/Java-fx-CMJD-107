101c02038866a79e15e8475d9d124fd4589935c5
NeranjayaDilshan <neranjayadilshan@gmail.com>
2024-07-14 16:51:43 +0530

    add db Connection

---
+++ database.sql
@@ -0,0 +1,11 @@
+Drop DATABASE kade;
+CREATE DATABASE kade;
+USE kade;
+
+CREATE TABLE Customer(
+	id VARCHAR(6) NOT NULL primary key,
+	name VARCHAR(30),
+	address VARCHAR(30) NOT NULL,
+	salary decimal(10,2) not null
+ 
+);

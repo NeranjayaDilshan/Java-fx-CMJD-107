0f9297fcbd1017f91fea599f2dc048b458bb0267
NeranjayaDilshan <neranjayadilshan@gmail.com>
2024-07-14 16:07:23 +0530

    on Action Customer

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,5 +1,26 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class CustomerController {
-    
+     
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtCustomerID;
+
+    @FXML
+    private TextField txtCustomerName;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void btnSaveCustomerOnAction(ActionEvent event) {
+
+    }
+
 }

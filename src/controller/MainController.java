a6997793e9194631347d13dee1c7d960466a6623
NeranjayaDilshan <neranjayadilshan@gmail.com>
2024-07-14 15:42:08 +0530

    load Customer UI

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,12 +1,25 @@
 package controller;
 
+import java.io.IOException;
+import java.net.URL;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MainController {
     @FXML
-    void btnCustomerOnAction(ActionEvent event) {
+    void btnCustomerOnAction(ActionEvent event) throws IOException {
         System.out.println("Customer");
+        URL resource = this.getClass().getResource("/view/Customer.fxml");
+        Parent node = FXMLLoader.load(resource);
+        Stage stage = new Stage();
+        stage.setScene(new Scene(node));
+        stage.show();
+        stage.setTitle("Customer Form");
     }
 
     @FXML
